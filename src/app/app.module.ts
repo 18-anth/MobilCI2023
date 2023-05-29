c1942bfbe068a016271f6fe4ee6777748a753521
Anthony Cordova <anthonycordova330@gmail.com>
2023-05-29 18:41:08 -0500

    Fin sección 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { PersonasComponent } from './personas/personas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonaComponent,
+    PersonasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
